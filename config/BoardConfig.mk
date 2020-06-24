8b4d2c93b600bf4b7cca58477a68988bd5516bb0
bunnyyTheFreak <hsinghalk@yahoo.com>
2020-06-24 03:05:09 +0000

    vendor: LockscreenCharging: Fix lockscreen charging current value
    Change-Id: I7df1166488505d2b64edf159ee89ee9c1702f752
    Signed-off-by: bunnyyTheFreak <hsinghalk@yahoo.com>

--- config/BoardConfig.mk
+++ config/BoardConfig.mk
@@ -16,3 +16,6 @@ endif
 TARGET_FWK_DETECT_PATH ?= hardware/qcom-caf/common
 PRODUCT_SOONG_NAMESPACES += \
     $(TARGET_FWK_DETECT_PATH)/fwk-detect
+
+# Enable real time lockscreen charging current values
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
