cdc3c11e2fe870ea821b8b8c2867161c490441ca
bunnyyTheFreak <hsinghalk@yahoo.com>
2020-08-08 08:00:56 +0530

    vendor: Fix the flag for faceunlock
    Change-Id: If95ac1a5ca2768ba0ba9f895a60106303954c421
    Signed-off-by: bunnyyTheFreak <hsinghalk@yahoo.com>

--- config/common.mk
+++ config/common.mk
@@ -219,7 +219,7 @@ endif
 # Face Unlock
 TARGET_FACE_UNLOCK_SUPPORTED := false
 ifeq ($(CUSTOM_BUILD_TYPE), OFFICIAL)
-ifeq (,$(filter $(TARGET_ARCH), arm64))
+ifeq ($(TARGET_ARCH), $(filter $(TARGET_ARCH), arm64))
 ifneq ($(TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK), true)
 PRODUCT_PACKAGES += \
     FaceUnlockService
